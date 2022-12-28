572b394e7d6bc1c973c9c3ecdde8a233054b3ce1
Aknaidu <muppeanand24@gmail.com>
2022-12-28 12:33:42 +0530

    one more branch added

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ class Demo
 public static void main(String args[])
 {
 System.out.println("created one empty repository");
+System.out.println("new line added");
 }
 }
---
+++ Demo1.java
@@ -0,0 +1,7 @@
+class Demo1
+{
+public satic void main(String args[])
+{
+System.out.println("New branch created");
+}
+}
